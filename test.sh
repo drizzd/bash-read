d914e089e07ec087854088133bb74b12e99efbe2
Clemens Buchacher <drizzd@gmx.net>
2018-10-03 10:58:33 +0200

    enable optimization

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -f seq seq.exe read read.exe &&
-gcc seq.c -oseq &&
-gcc read.c -oread &&
-time sh -c './seq 60000 | ./read'
+gcc -O2 seq.c -oseq &&
+gcc -O2 read.c -oread &&
+time sh -c './seq 10000 | ./read'
