7f600574f04ae91602207a830487fb4c7186b47b
Clemens Buchacher <drizzd@gmx.net>
2018-10-03 11:02:36 +0200

    fix indentation

--- read.c
+++ read.c
@@ -5,16 +5,15 @@
 char buf[BUFSIZE];
 int main()
 {
-        while (1) {
-                int i;
-                i = read(0, buf, BUFSIZE);
-                if (i < 0) {
-                        perror("read");
-                        exit(1);
-                } else if (i == 0) {
-                        break;
-                }
-                //fprintf(stdout, "%d\n", i);
-        }
-        return 0;
+	while (1) {
+		int i;
+		i = read(0, buf, BUFSIZE);
+		if (i < 0) {
+			perror("read");
+			exit(1);
+		} else if (i == 0) {
+			break;
+		}
+	}
+	return 0;
 }
